6f7160201cac359e8c164e41fcc16a9f9a36ce3a
devssabbir <heartedsabbirdhrubo@gmail.com>
2022-03-22 22:01:29 +0600

    Up

---
+++ app.js
@@ -0,0 +1,73 @@
+import Storage from "./src/Storage.js"
+
+
+const add_form = document.querySelector('#add_form')
+const loadData = document.querySelector('#loadData')
+
+
+add_form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const form_data = new FormData(e.target)
+    const form_entry = Object.fromEntries(form_data.entries())
+
+    const {
+        name,
+        cel,
+        photo,
+        location
+    } = form_entry
+
+    if (name == '' || cel == '' || photo == '' || location == '') {
+        alert('All Feild Are Required !')
+    } else {
+
+        Storage.set('Devs', form_entry)
+        getAlldata()
+        add_form.reset()
+    }
+})
+
+
+function getAlldata() {
+    let data = Storage.get('Devs')
+    let titem = '';
+    data.map((data, index) => {
+        let {
+            name,
+            cel,
+            photo,
+            location
+        } = data
+        titem += `
+        <tr>
+
+           <td>${index + 1}</td> 
+           <td>${name}</td>
+           <td>${cel}</td>
+           <td>${photo}</td>
+           <td>${location}</td>
+           <td>
+             <button class="btn-close" onclick="alert(${ index })"></button>
+           </td>
+        
+        </tr>
+           
+        `
+    })
+
+    loadData.innerHTML = titem;
+}
+
+getAlldata()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Storage.js
@@ -0,0 +1,25 @@
+class Storage{
+
+  static has(key){   
+      return localStorage.getItem(key) ? localStorage.getItem(key) : false;
+        
+    }
+
+  
+    static set(key,data){
+       let setData = [];
+       if(this.has(key)){
+         setData = JSON.parse(this.has(key))  
+       }
+
+       setData.push(data)
+
+       localStorage.setItem(key,JSON.stringify(setData))
+    }
+
+    static get(key){
+       return JSON.parse(this.has(key)) 
+    }
+}
+
+export default Storage;
